c52ae22ba31d6d2142d3b80c273ced1cc5ce493d
nshush <kagekao137@gmail.com>
2024-10-10 21:36:53 +0300

    Add file for exercise 3.3.2

---
+++ Task332.cs
@@ -0,0 +1,14 @@
+using System;
+
+public static void Main(string[] args)
+{
+    string name = "Jane";
+    byte c = 27;
+    bool haveApet = true;
+    double shoeSize = 37.5;
+
+    Console.WriteLine("My name is " + name);
+    Console.WriteLine("MyAge " + name);
+    Console.WriteLine("Do I have a pet? " + haveApet);
+    Console.WriteLine("My shoe size is " + shoeSize);
+}
